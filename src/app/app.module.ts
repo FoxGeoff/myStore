e093dec43319245ab7dffcb04623205460575bd0
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-25 13:25:49 -0700

    Add Search component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,6 +27,7 @@ import { SearchComponent } from './search/search.component';
       { path: '', component: HomeComponent },
       { path: 'products/:productId', component: ProductDetailComponent }
     ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
